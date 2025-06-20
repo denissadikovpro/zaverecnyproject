445715190c9e950169cb506d89008c0aec54098b
denissadikovpro <volodymyr.klimuk@student.ukf.sk>
2025-06-20 20:44:13 +0300

    created reviews.php

---
+++ reviews.php
@@ -0,0 +1,57 @@
+<!-- HTML starts here -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+  <title>Reviews</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Additional CSS Files -->
+  <link rel="stylesheet" href="assets/css/fontawesome.css">
+  <link rel="stylesheet" href="assets/css/templatemo-plot-listing.css">
+  <link rel="stylesheet" href="assets/css/animated.css">
+  <link rel="stylesheet" href="assets/css/owl.css">
+</head>
+
+<body>
+
+<!-- Include header -->
+<?php include_once "assets/parts/header.php" ?>
+
+<!-- banner section starts -->
+  <div class="page-heading">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8">
+          <div class="top-text header-text">
+            <h6>Share your thoughts with us</h6>
+            <h2>Feel free to leave a review and let us know what you think about our services</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<!-- banner section ends -->
+
+
+
+
+<?php include_once "assets/parts/footer.php" ?>
+
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/owl-carousel.js"></script>
+<script src="assets/js/animation.js"></script>
+<script src="assets/js/imagesloaded.js"></script>
+<script src="assets/js/custom.js"></script>
+
+</body>
+</html>
