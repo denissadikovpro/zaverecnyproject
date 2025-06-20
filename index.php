4338bdda6bbeea06e5c9c4bd0377c7ed1618b65b
denissadikovpro <volodymyr.klimuk@student.ukf.sk>
2025-06-20 20:31:46 +0300

    Main banner section added

--- index.php
+++ index.php
@@ -32,6 +32,73 @@
 
 
 
+<!-- main banner section starts -->
+<div class="main-banner">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="top-text header-text">
+            <h6>Over 36,500+ Active Listings</h6>
+            <h2>Find Nearby Places &amp; Things</h2>
+          </div>
+        </div>
+        <div class="col-lg-12">
+          <form id="search-form" name="gs" method="submit" role="search" action="#">
+            <div class="row">
+              <div class="col-lg-3 align-self-center">
+                  <fieldset>
+                      <select name="area" class="form-select" aria-label="Area" id="chooseCategory" onchange="this.form.click()">
+                          <option selected>All Areas</option>
+                          <option value="Village">Village</option>
+                          <option value="Town">Town</option>
+                          <option value="Modern City">Modern City</option>
+                          <option value="Countryside">Countryside</option>
+                          <option value="Desert">Desert</option>
+                      </select>
+                  </fieldset>
+              </div>
+              <div class="col-lg-3 align-self-center">
+                  <fieldset>
+                      <input type="address" name="address" class="searchText" placeholder="Enter a location" autocomplete="on" required>
+                  </fieldset>
+              </div>
+              <div class="col-lg-3 align-self-center">
+                  <fieldset>
+                      <select name="price" class="form-select" aria-label="Default select example" id="chooseCategory" onchange="this.form.click()">
+                          <option selected>Price Range</option>
+                          <option value="$100 - $250">$100 - $250</option>
+                          <option value="$250 - $500">$250 - $500</option>
+                          <option value="$500 - $1000">$500 - $1,000</option>
+                          <option value="$1000+">$1,000 or more</option>
+                      </select>
+                  </fieldset>
+              </div>
+              <div class="col-lg-3">                        
+                  <fieldset>
+                      <button class="main-button"><i class="fa fa-search"></i> Search Now</button>
+                  </fieldset>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div class="col-lg-10 offset-lg-1">
+          <ul class="categories">
+            <li><a href="category.php"><span class="icon"><img src="assets/images/search-icon-01.png" alt="Home"></span>Apartments</a></li>
+            <li><a href="listing.php"><span class="icon"><img src="assets/images/search-icon-02.png" alt="Food"></span>Food</a></li>
+            <li><a href="#"><span class="icon"><img src="assets/images/search-icon-03.png" alt="Vehicle"></span>Car rental</a></li>
+            <li><a href="#"><span class="icon"><img src="assets/images/search-icon-04.png" alt="Shopping"></span>Shopping</a></li>
+            <li><a href="#"><span class="icon"><img src="assets/images/search-icon-05.png" alt="Travel"></span>Tours</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+<!-- main banner section ends -->
+
+
+
+
+
 <!-- footer section starts -->
 <?php include_once "assets/parts/footer.php" ?>
 <!-- footer section ends -->
