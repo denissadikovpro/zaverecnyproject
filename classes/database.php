f0d1a7e3bffc1e20da61d2779a6bb390432a372a
denissadikovpro <volodymyr.klimuk@student.ukf.sk>
2025-06-20 20:36:35 +0300

    file where PDO connects database and PHP code

--- classes/database.php
+++ classes/database.php
@@ -6,4 +6,15 @@ class Database {
     private $username = "root";
     private $password = "";
     public $conn; // Holds PDO connection object
-    
+
+    public function connect() {
+        $this->conn = null;
+        try {  // Create new PDO connection
+            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Enable PDO error exceptions
+        } catch(PDOException $e) {
+            echo "Connection error: " . $e->getMessage(); // Print connection error
+        }
+        return $this->conn; // Return PDO connection object
+    }
+}
