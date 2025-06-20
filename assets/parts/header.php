910a8bb00254ac0254f119e98f8b0436cc63f65c
denissadikovpro <volodymyr.klimuk@student.ukf.sk>
2025-06-20 20:27:31 +0300

    Created footer.php and header.php

---
+++ assets/parts/header.php
@@ -0,0 +1,26 @@
+<header class="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <nav class="main-nav">
+            <!-- ***** Logo Start ***** -->
+            <a href="index.php" class="logo">
+            </a>
+            <!-- ***** Logo End ***** -->
+            <!-- ***** Menu Start ***** -->
+            <ul class="nav">
+              <li><a href="index.php">Home</a></li>
+              <li><a href="category.php">Category</a></li>
+              <li><a href="listing.php">Listing</a></li>
+              <li><a href="contact.php">Contact Us</a></li> 
+              <li><a href="reviews.php">Reviews</a></li>
+            </ul>        
+            <a class='menu-trigger'>
+                <span>Menu</span>
+            </a>
+            <!-- ***** Menu End ***** -->
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
