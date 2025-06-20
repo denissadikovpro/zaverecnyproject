92808a472d3fd0aaf9025d543580b394275b0936
denissadikovpro <volodymyr.klimuk@student.ukf.sk>
2025-06-20 20:42:01 +0300

    Added basic things, included footer and header

--- contact.php
+++ contact.php
@@ -69,3 +69,76 @@ $pdo = $db->connect();
 $controller = new ContactController($pdo);
 $controller->handleRequest();
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Improves loading performance for Google Fonts by preconnecting -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+    <title>Contact us</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-plot-listing.css">
+    <link rel="stylesheet" href="assets/css/animated.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+
+  </head>
+
+<body>
+
+
+<!-- header section starts -->
+<?php include_once "assets/parts/header.php" ?>
+<!-- header section ends -->
+
+
+<!-- contact banner section starts -->
+  <div class="page-heading">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8">
+          <div class="top-text header-text">
+            <h6>Keep in touch with us</h6>
+            <h2>Feel free to send us a message about your business needs</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<!-- contact banner section ends -->
+
+
+
+
+
+
+<!-- footer section starts -->
+<?php include_once "assets/parts/footer.php" ?>
+<!-- footer section ends -->
+
+
+  <!-- Scripts -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/js/owl-carousel.js"></script>
+  <script src="assets/js/animation.js"></script>
+  <script src="assets/js/imagesloaded.js"></script>
+  <script src="assets/js/custom.js"></script>
+  
+</body>
+</html>
