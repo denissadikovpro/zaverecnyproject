8275484baa1b6e73ccc365722faa42f76c74a8d1
denissadikovpro <volodymyr.klimuk@student.ukf.sk>
2025-06-20 20:26:49 +0300

    Created footer.php

---
+++ footer.php
@@ -0,0 +1,57 @@
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="about">
+            <div class="logo">
+              <img src="assets/images/black-logo.png" alt="Plot Listing Template">
+            </div>
+            <p>Let us take care of your stay, your ride, and your adventures.</p>
+            <p>Explore. Rent. Shop. Travel.</p>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="helpful-links">
+            <h4>Helpful Links</h4>
+            <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                  <li><a href="#">Categories</a></li>
+                  <li><a href="#">Reviews</a></li>
+                  <li><a href="#">Listing</a></li>
+                  <li><a href="#">Contact Us</a></li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul>
+                  <li><a href="#">About Us</a></li>
+                  <li><a href="#">Awards</a></li>
+                  <li><a href="#">Useful Sites</a></li>
+                  <li><a href="#">Privacy Policy</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="contact-us">
+            <h4>Contact Us</h4>
+            <p>27th Street of New Town, Digital Villa</p>
+            <div class="row">
+              <div class="col-lg-6">
+                <a href="#">0952-234-781</a>
+              </div>
+              <div class="col-lg-6">
+                <a href="#">0952-234-546</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-12">
+          <div class="sub-footer">
+            <p>Copyright © 2025 Plot Listing Co., Ltd. All Rights Reserved.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
